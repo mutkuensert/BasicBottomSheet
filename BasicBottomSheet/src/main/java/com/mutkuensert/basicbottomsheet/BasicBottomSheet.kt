3fa25c13fc839b1334f48a05982074e0a3839807
M. Utku Ensert <mutkuensert@hotmail.com>
2024-02-28 18:16:32 +0300

    Remove windowinsetspadding modifier.

--- BasicBottomSheet/src/main/java/com/mutkuensert/basicbottomsheet/BasicBottomSheet.kt
+++ BasicBottomSheet/src/main/java/com/mutkuensert/basicbottomsheet/BasicBottomSheet.kt
@@ -21,15 +21,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
@@ -96,7 +93,6 @@ fun BasicBottomSheet(
                     modifier = Modifier
                         .fillMaxSize()
                         .background(containerColor)
-                        .windowInsetsPadding(WindowInsets.systemBars)
                         .clickable(onClick = onCloseSheet)
                 )
 
